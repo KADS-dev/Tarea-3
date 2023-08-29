1ecc0bcbc61c87f8d4bd066eee500b3a287fd0d0
Kevin Alexandr <kevinalexandr@hotmail.com>
2023-08-28 19:52:39 -0600

    Subi programa 1

---
+++ Modus Ponens.py
@@ -0,0 +1,8 @@
+#Ejemplo método de inferencia modus Ponens:
+print("Metodo Ponens:")
+print("¿Está lloviendo?s/n")
+estadoLLoviendo=input()
+if estadoLLoviendo=="s":
+    print("La calle esta mojada")
+else:
+    print("La calle no está mojada")
---
+++ Modus Tollens.py
@@ -0,0 +1,8 @@
+#Ejemplo método de inferencia modus Tollens:
+print("Metodo Ponens:")
+print("¿El pasto está humedo?s/n")
+estadoLLoviendo=input()
+if estadoLLoviendo=="s":
+    print("Se ha regado el pasto")
+else:
+    print("No se ha regado el pasto")
